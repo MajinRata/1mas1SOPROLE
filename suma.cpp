56a568c07ba14a8007a642a495f4a95420dca3d4
MajinRata <seimirchaplin@outlook.com>
2016-05-14 22:00:19 -0300

    Update and rename suma.cpp to suma.py

--- suma.cpp
+++
@@ -1,8 +0,0 @@
-#include <cstdio>
-int main() {
-  int a, b;
-  printf("Elige dos números que quieras sumar: ");
-  scanf("%d%d", &a, &b);
-  printf("El resultado es: ", a+b);
-  return 0;
-}
---
+++ suma.py
@@ -0,0 +1,4 @@
+print("Ingresa dos números que quieras sumar:")
+a = int(input())
+b = int(input())
+print(a+b)
